356082c5ff522504977d9899e443899847dc11e8
hussainshamim16 <hj422748@gmail.com>
2025-01-25 18:24:23 +0500

    Remove unused dependencies and clean up related files

--- FRONTEND/src/routes/Approutes.jsx
+++ FRONTEND/src/routes/Approutes.jsx
@@ -3,14 +3,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from '../pages/Home';
 import About from '../pages/About';
 import Contact from '../pages/Contact';
+import Users from '../application api/Users';
 
 const Approutes = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home/>} />
+                <Route path='/' element={<Home />} />
                 <Route path='/about' element={<About />} />
                 <Route path='/contact' element={<Contact />} />
+                <Route path='/users' element={<Users />} />
             </Routes>
         </BrowserRouter>
     )
