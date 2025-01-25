1cbcd6f52a8d12cd11207d5cea2fee8bf2758252
hussainshamim16 <hj422748@gmail.com>
2025-01-25 17:31:27 +0500

    configure store using React-Redux

--- FRONTEND/src/pages/Home.jsx
+++ FRONTEND/src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from 'react'
+import { useSelector, useDispatch } from 'react-redux' // read and update karney key liye 
+import { decrement, increment } from '../Store/counter/counterSlice' // store key functions
 
 const Home = () => {
+    const count = useSelector((state) => state.counter.value)
+    const dispatch = useDispatch()
     const greating = (e) => {
         alert(`${e} Hussain`)
     }
     return (
         <>
-            <h1>Home</h1>
-            <button onClick={()=>greating("kia ho rha hey ")} className='btn'>clickmei</button>
+            <h1>Home {count}</h1>
+            <button onClick={() => dispatch(increment())} className='btn'>Plus</button>
+            <button onClick={() => dispatch(decrement())} className='btn'>Minus</button>
         </>
     )
 }
