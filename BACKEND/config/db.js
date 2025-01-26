f927301c0b721088efa6d5a6f60d1df03de6e930
hussainshamim16 <hj422748@gmail.com>
2025-01-26 18:42:24 +0500

    deploy error solve

--- BACKEND/config/db.js
+++ BACKEND/config/db.js
@@ -1,9 +1,11 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config()
 
 const connectDB = async () => {
   try {
     const connectionInstance = await mongoose.connect(
-      `${"mongodb+srv://hussainshamim:jP46GY6fv3Phn6jj@cluster0.o2whh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"}todo`
+      `${process.env.MONGO_URI}todo`
     );
     console.log(
       `\n MongoDB connected`
